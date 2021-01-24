519fbc14309c83a3c7f1a438856e3d61da96bca8
Michal Sobolewski <msobdev@gmail.com>
2021-01-24 17:55:57 +0100

    Initial commit

---
+++ src/main/java/com/msobdev/kafka/restkafka/RestKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.msobdev.kafka.restkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestKafkaApplication.class, args);
+	}
+
+}
